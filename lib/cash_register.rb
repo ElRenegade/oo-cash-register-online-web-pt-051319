8822702cd395a7e67f7583267dbc05bab1328165
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-10 01:47:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,12 +7,15 @@ class CashRegister
     @discount = discount
   end
 
+  def total
+    @total
+  end
+
   def add_item(title, price, quantity = 1)
     @total = price * quantity
     previous_total = @total
     @items << title
     @total
-
   end
 
 end
